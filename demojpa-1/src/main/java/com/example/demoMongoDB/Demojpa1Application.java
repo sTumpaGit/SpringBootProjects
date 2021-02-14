08eeb591b19cc9f7ed1ec44266d4e1540ea0bc7c
Tumpa sheet <68402283+sTumpaGit@users.noreply.github.com>
2021-02-14 22:21:24 +0530

    Add files via upload
    Spring Boot JPA H2 Demo

---
+++ demojpa-1/src/main/java/com/example/demoMongoDB/Demojpa1Application.java
@@ -0,0 +1,14 @@
+package com.example.demoMongoDB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@SpringBootApplication
+public class Demojpa1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Demojpa1Application.class, args);
+	}
+
+}
